10cf764b50a8048eda9e601320d48b61a50c90e1
Vlad Burnaev <vlad_burn@icloud.com>
2020-09-13 02:45:03 +0300

    Data buttons

--- src/App.js
+++ src/App.js
@@ -1,80 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import Table from "./Table/Table";
-import _ from 'lodash';
+
 
 function App() {
-    const smallRequestURL = 'http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
-    useEffect(() => {
-        fetch(smallRequestURL)
-            .then(response => response.json())
-            .then(persons => {
-                setPersons(_.orderBy(persons, 'id', 'asc'))
-            })
-    }, []);
-
-    const [persons, setPersons] = useState([]);
-    const [prevPersons, setPrevPersons] = useState([]);
-    const [sortTo, setSortTo] = useState('asc');
-    const [sortField, setSortField] = useState('id');
-    const [page, setPage] = useState(1);
-
-    const tableHeader = ['id', 'firstName', 'lastName', 'email', 'phone'];
-    const rowsPerPage = 50;
-    const numberOfPages = Math.ceil(persons.length / rowsPerPage);
-    const pagesIntervals = takeIntervals(rowsPerPage, numberOfPages);
-
-    function takeIntervals(nRows, nPages) {
-        const intervals = [];
-        let start = 0;
-        let end = nRows-1;
-        for (let i=0; i<=nPages; i++) {
-            intervals[i]=[start, end];
-            start+=nRows;
-            end+=nRows;
-        }
-        return intervals;
-    }
-    function sortTable(sortField) {
-        const copyPersons = persons.concat();
-        const sortType = sortTo === 'asc' ? 'desc' : 'asc';
-        const sortPersons = _.orderBy(copyPersons, sortField, sortType);
-        setSortField(sortField);
-        setPersons(sortPersons);
-        setSortTo(sortType);
-    }
-    function filterTable(value) {
-        const copyPersons = persons.concat();
-        setPrevPersons(copyPersons);
-        if (value !== '') {
-            const filterPersons = _.filter(copyPersons, function (o) {
-                if (o.firstName.includes(value) || o.lastName.includes(value) || o.email.includes(value) || o.phone.includes(value)) {
-                    return true;
-                } else return false;
-            })
-            setPersons(filterPersons);
-        } else {
-            setPersons(prevPersons);
-        }
-        ;
-
-    }
-
+    const smallRequestURL = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+    const largeRequestURL = 'http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+    const [showSmall, setShowSmall] = useState(false);
+    const [showLarge, setShowLarge] = useState(false);
     return (
         <div className="app-wrapper">
             <h1>Тестовое задание</h1>
-            <button>Small dataset</button>
-            &nbsp;
-            <button>Large dataset</button>
-            <Table persons={persons}
-                   tableHeader={tableHeader}
-                   sortTable={sortTable}
-                   filterTable={filterTable}
-                   sortTo={sortTo}
-                   sortField={sortField}
-                   numberOfPages={numberOfPages}
-                   pagesIntervals={pagesIntervals}
-                   page={page}
-                   setPage={setPage}/>
+            <button onClick={() => {setShowSmall(true);setShowLarge(false);}}>Small dataset</button>
+            <button onClick={() => {setShowLarge(true);setShowSmall(false);}}>Large dataset</button>
+            {showSmall && <Table requestURL={smallRequestURL}/>}
+            {showLarge && <Table requestURL={largeRequestURL}/>}
         </div>
     );
 }
--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from './Table.module.css'
 import Rows from "./Rows/Rows";
 import Head from "./Head/Head";
 import Filter from "./Filter/Filter";
 import Pagination from "./Pagination/Pagination";
+import _ from "lodash";
 
 export default function Table(props) {
-    const {tableHeader, persons, sortTable, filterTable, sortTo, sortField, numberOfPages, pagesIntervals, page, setPage} = props;
+    const {requestURL} = props;
+    useEffect(() => {
+        fetch(requestURL)
+            .then(response => response.json())
+            .then(persons => {
+                setPersons(_.orderBy(persons, 'id', 'asc'))
+            })
+    }, []);
+
+    const [persons, setPersons] = useState([]);
+    const [prevPersons, setPrevPersons] = useState([]);
+    const [sortTo, setSortTo] = useState('asc');
+    const [sortField, setSortField] = useState('id');
+    const [page, setPage] = useState(1);
+
+    const tableHeader = ['id', 'firstName', 'lastName', 'email', 'phone'];
+    const rowsPerPage = 50;
+    const numberOfPages = Math.ceil(persons.length / rowsPerPage);
+    const pagesIntervals = takeIntervals(rowsPerPage, numberOfPages);
+
+    function takeIntervals(nRows, nPages) {
+        const intervals = [];
+        let start = 0;
+        let end = nRows-1;
+        for (let i=0; i<=nPages; i++) {
+            intervals[i]=[start, end];
+            start+=nRows;
+            end+=nRows;
+        }
+        return intervals;
+    }
+    function sortTable(sortField) {
+        const copyPersons = persons.concat();
+        const sortType = sortTo === 'asc' ? 'desc' : 'asc';
+        const sortPersons = _.orderBy(copyPersons, sortField, sortType);
+        setSortField(sortField);
+        setPersons(sortPersons);
+        setSortTo(sortType);
+    }
+    function filterTable(value) {
+        const copyPersons = persons.concat();
+        setPrevPersons(copyPersons);
+        if (value !== '') {
+            const filterPersons = _.filter(copyPersons, function (o) {
+                if (o.firstName.includes(value) || o.lastName.includes(value) || o.email.includes(value) || o.phone.includes(value)) {
+                    return true;
+                } else return false;
+            })
+            setPersons(filterPersons);
+        } else {
+            setPersons(prevPersons);
+        }
+        ;
+
+    }
 
     return (
         <>
