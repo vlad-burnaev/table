39a4469f6c77025fae36fd5b3ee54113c7c06e02
Vlad Burnaev <vlad_burn@icloud.com>
2020-10-21 13:50:11 +0300

    Updates

---
+++ precache-manifest.bc1195e6312583ffa8ccfd32c5ebda2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf45f3ab245032d0b4eb70411f031936",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "8caa649509208c61b606",
+    "url": "/table/static/css/main.178df8dc.chunk.css"
+  },
+  {
+    "revision": "52b20294f8aba5a7699a",
+    "url": "/table/static/js/2.63aab487.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/table/static/js/2.63aab487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8caa649509208c61b606",
+    "url": "/table/static/js/main.ae59da18.chunk.js"
+  },
+  {
+    "revision": "479ff0febc43ec3ae7b9",
+    "url": "/table/static/js/runtime-main.b436abf5.js"
+  }
+]);
